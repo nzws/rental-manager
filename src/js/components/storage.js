c3f8270ab1e7985ad3a18b68ae514a88681239b3
nzws <git@nzws.me>
2019-05-29 20:24:17 +0900

    1.2.0 (#1)
    * wip
    
    * wip

--- src/js/components/storage.js
+++ src/js/components/storage.js
@@ -13,9 +13,17 @@ export default {
   delete(key) {
     return config.delete(key);
   },
-  clear() {
-    if (!confirm('よろしいですか？')) return;
+  clearAll() {
+    if (!confirm('"全てのデータ"を削除します。よろしいですか？（削除後再ログインが必要になります。）')) return;
+    if (!confirm('まじ？')) return;
+
     config.clear();
+    move.move('./index.html');
+  },
+  clear(type) {
+    if (!confirm(`${type} の全削除を行います。よろしいですか？（削除後再ログインが必要になります。）`)) return;
+
+    this.delete(type);
     move.move('./index.html');
   }
 };
